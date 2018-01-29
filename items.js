3f3fc384bb245f25df852685702049bd6c40bc60
pdhaval <dhavalpatel738@gmail.com>
2018-01-29 21:32:15 +0530

    remove row logic from search.html

--- items.js
+++ items.js
@@ -1,17 +1,17 @@
 var items = [
     {
         label: "37281",
-        image_sm: "Images/LightDarkSeries/37281-sm.jpg",
-        image_l: "Images/LightDarkSeries/37281-l.jpeg"
+        image_sm: "Images/LightDarkSeries/37247 HL1.jpg",
+        image_lg: "Images/LightDarkSeries/37247 HL1.jpg"
     },
     {
         label: "37282",
-        image_sm: "Images/LightDarkSeries/37281-sm.jpg",
-        image_l: "Images/LightDarkSeries/37281-l.jpeg"
+        image_sm: "Images/LightDarkSeries/37281 HL1-sm.jpg",
+        image_lg: "Images/LightDarkSeries/37281 HL1-lg.jpg"
     },
     {
         label: "37283",
         image_sm: "Images/LightDarkSeries/37281-sm.jpg",
-        image_l: "Images/LightDarkSeries/37281-l.jpeg"
+        image_lg: "Images/LightDarkSeries/37281-lg.jpg"
     }
 ]
