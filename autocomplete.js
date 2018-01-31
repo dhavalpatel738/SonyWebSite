c5c2962ea4c5c67017c79d070f599524398c41eb
pdhaval <dhavalpatel738@gmail.com>
2018-01-31 10:23:38 +0530

    new name in json & one result in search

--- autocomplete.js
+++ autocomplete.js
@@ -3,18 +3,22 @@ $(function() {
                   minLength: 0,
                   source: items,
                   focus: function( event, ui ) {
-                    $( "#search" ).val( ui.item.label );
+                    $( "#search" ).val( ui.item.name );
                     return false;
                   },
                   select: function( event, ui ) {
-                    $( "#search" ).val( ui.item.label );            
+                    $( "#search" ).val( ui.item.name );            
                     return false;
                   }
                 })
                 .autocomplete( "instance" )._renderItem = function( ul, item ) {
-                  return $( "<li>" )
-                    .append( "<div>" + item.label + "</div>" )
+                 if(item.name == "dup") {
+                    return $("<li>");
+                } else {
+                    return $( "<li>" )
+                    .append( "<div>" + item.name + "</div>" )
                     .appendTo( ul );
+                }
                 };
 } );
 
--- items.js
+++ items.js
@@ -1,16 +1,25 @@
 var items = [
     {
         label: "37281",
+        name: "37281",
         image_sm: "Images/LightDarkSeries/37263 HL1.jpg",
         image_lg: "Images/LightDarkSeries/37263 HL1.jpg"
     },
     {
         label: "3001 HL1 HL2",
+        name: "3001 HL1 HL2",
         image_sm: "Images/LightDarkSeries/37281 HL1-sm.jpg",
         image_lg: "Images/LightDarkSeries/37281 HL1-lg.jpg"
     },
     {
         label: "37281",
+        name: "dup",
+        image_sm: "Images/LightDarkSeries/3001 HL1 HL2.jpg",
+        image_lg: "Images/LightDarkSeries/3001 HL1 HL2.jpg"
+    },
+    {
+        label: "37281",
+        name: "dup",
         image_sm: "Images/LightDarkSeries/3001 HL1 HL2.jpg",
         image_lg: "Images/LightDarkSeries/3001 HL1 HL2.jpg"
     }
