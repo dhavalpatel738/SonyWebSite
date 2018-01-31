6db8cc56713a386c379262d7b3a2d2bcadd573a2
pdhaval <dhavalpatel738@gmail.com>
2018-01-31 11:12:28 +0530

    Disable right click option
    1. Disable context menu on all pages
    2. format autocomplete.js
    3. Seach placeholder to 'Search Design'

--- autocomplete.js
+++ autocomplete.js
@@ -1,24 +1,28 @@
 $(function() {
-             $( "#search" ).autocomplete({
-                  minLength: 0,
-                  source: items,
-                  focus: function( event, ui ) {
-                    $( "#search" ).val( ui.item.name );
-                    return false;
-                  },
-                  select: function( event, ui ) {
-                    $( "#search" ).val( ui.item.name );            
-                    return false;
-                  }
-                })
-                .autocomplete( "instance" )._renderItem = function( ul, item ) {
-                 if(item.name == "dup") {
-                    return $("<li>");
-                } else {
-                    return $( "<li>" )
+    $( "#search" ).autocomplete({
+        minLength: 0,
+        source: items,
+        focus: function( event, ui ) {
+            $( "#search" ).val( ui.item.name );
+                return false;
+        },
+        select: function( event, ui ) {
+            $( "#search" ).val( ui.item.name );
+                return false;
+        }
+    })
+        .autocomplete( "instance" )._renderItem = function( ul, item ) {
+            if(item.name == "dup") {
+                return $("<li>");
+            } else {
+                return $( "<li>" )
                     .append( "<div>" + item.name + "</div>" )
                     .appendTo( ul );
-                }
-                };
+            }
+        };
+
+    $("body").on("contextmenu",function(){
+        return false;
+    });
 } );
 
