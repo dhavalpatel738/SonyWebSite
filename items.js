322054c35aece01c08b5aa6ad9a18909e50e657b
pdhaval <dhavalpatel738@gmail.com>
2018-01-31 22:01:27 +0530

    LD list in items.js

--- items.js
+++ items.js
@@ -185,5 +185,403 @@ var items = [
 "image_lg":"Images/ElevationSeries/375045-lg.jpg",
 "name":"375045",
 "label":"375045"
+},
+    
+    //LightDarkSeries
+{
+"image_sm":"Images/LightDarkSeries/37236 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37236 HL1-sm.jpg",
+"name":"37236",
+"label":"37236"
+},
+{
+"image_sm":"Images/LightDarkSeries/37253 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37253 HL1 HL2-sm.jpg",
+"name":"37253",
+"label":"37253"
+},
+{
+"image_sm":"Images/LightDarkSeries/37263 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37263 HL1-sm.jpg",
+"name":"37263",
+"label":"37263"
+},
+{
+"image_sm":"Images/LightDarkSeries/37185 HL4-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37185 HL4-sm.jpg",
+"name":"37185",
+"label":"37185"
+},
+{
+"image_sm":"Images/LightDarkSeries/3001 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/3001 HL1 HL2-lg.jpg",
+"name":"3001",
+"label":"3001"
+},
+{
+"image_sm":"Images/LightDarkSeries/37281 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37281 HL2-lg.jpg",
+"name":"dup",
+"label":"37281"
+},
+{
+"image_sm":"Images/LightDarkSeries/37248 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37248 HL1-sm.jpg",
+"name":"37248",
+"label":"37248"
+},
+{
+"image_sm":"Images/LightDarkSeries/37249 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37249 HL1 HL2-sm.jpg",
+"name":"37249",
+"label":"37249"
+},
+{
+"image_sm":"Images/LightDarkSeries/37272 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37272 HL1-sm.jpg",
+"name":"37272",
+"label":"37272"
+},
+{
+"image_sm":"Images/LightDarkSeries/3037 HL1-4-sm.jpg",
+"image_lg":"Images/LightDarkSeries/3037 HL1-4-lg.jpg",
+"name":"3037",
+"label":"3037"
+},
+{
+"image_sm":"Images/LightDarkSeries/37276 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37276 HL2-lg.jpg",
+"name":"dup",
+"label":"37276"
+},
+{
+"image_sm":"Images/LightDarkSeries/37278-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37278-lg.jpg",
+"name":"37278",
+"label":"37278"
+},
+{
+"image_sm":"Images/LightDarkSeries/37240 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37240 HL1-sm.jpg",
+"name":"37240",
+"label":"37240"
+},
+{
+"image_sm":"Images/LightDarkSeries/3001 HL5-sm.jpg",
+"image_lg":"Images/LightDarkSeries/3001 HL5-lg.jpg",
+"name":"dup",
+"label":"3001"
+},
+{
+"image_sm":"Images/LightDarkSeries/3079 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/3079 HL1 HL2-lg.jpg",
+"name":"3079",
+"label":"3079"
+},
+{
+"image_sm":"Images/LightDarkSeries/37252 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37252 HL1 HL2-sm.jpg",
+"name":"37252",
+"label":"37252"
+},
+{
+"image_sm":"Images/LightDarkSeries/37261 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37261 HL1-sm.jpg",
+"name":"37261",
+"label":"37261"
+},
+{
+"image_sm":"Images/LightDarkSeries/37247 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37247 HL1-sm.jpg",
+"name":"37247",
+"label":"37247"
+},
+{
+"image_sm":"Images/LightDarkSeries/37151 HL2-4-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37151 HL2-4-lg.jpg",
+"name":"37151",
+"label":"37151"
+},
+{
+"image_sm":"Images/LightDarkSeries/37273 HL1-3-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37273 HL1-3-sm.jpg",
+"name":"37273",
+"label":"37273"
+},
+{
+"image_sm":"Images/LightDarkSeries/37118 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37118 HL2-lg.jpg",
+"name":"37118",
+"label":"37118"
+},
+{
+"image_sm":"Images/LightDarkSeries/37239 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37239 HL1-sm.jpg",
+"name":"37239",
+"label":"37239"
+},
+{
+"image_sm":"Images/LightDarkSeries/37242 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37242 HL1 HL2-sm.jpg",
+"name":"37242",
+"label":"37242"
+},
+{
+"image_sm":"Images/LightDarkSeries/37275 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37275 HL1-sm.jpg",
+"name":"37275",
+"label":"37275"
+},
+{
+"image_sm":"Images/LightDarkSeries/37281 HL6-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37281 HL6-lg.jpg",
+"name":"dup",
+"label":"37281"
+},
+{
+"image_sm":"Images/LightDarkSeries/37244 HL1-3-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37244 HL1-3-sm.jpg",
+"name":"37244",
+"label":"37244"
+},
+{
+"image_sm":"Images/LightDarkSeries/37114 HL3-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37114 HL3-lg.jpg",
+"name":"37114",
+"label":"37114"
+},
+{
+"image_sm":"Images/LightDarkSeries/37251 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37251 HL1 HL2-sm.jpg",
+"name":"37251",
+"label":"37251"
+},
+{
+"image_sm":"Images/LightDarkSeries/37259 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37259 HL1 HL2-sm.jpg",
+"name":"37259",
+"label":"37259"
+},
+{
+"image_sm":"Images/LightDarkSeries/7015 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/7015 HL1-lg.jpg",
+"name":"7015",
+"label":"7015"
+},
+{
+"image_sm":"Images/LightDarkSeries/37260 HL1-3-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37260 HL1-3-lg.jpg",
+"name":"37260",
+"label":"37260"
+},
+{
+"image_sm":"Images/LightDarkSeries/37270 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37270 HL1 HL2-sm.jpg",
+"name":"37270",
+"label":"37270"
+},
+{
+"image_sm":"Images/LightDarkSeries/37265 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37265 HL1 HL2-sm.jpg",
+"name":"37265",
+"label":"37265"
+},
+{
+"image_sm":"Images/LightDarkSeries/37187 HL3-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37187 HL3-sm.jpg",
+"name":"37187",
+"label":"37187"
+},
+{
+"image_sm":"Images/LightDarkSeries/37241 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37241 HL1-sm.jpg",
+"name":"37241",
+"label":"37241"
+},
+{
+"image_sm":"Images/LightDarkSeries/37276 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37276 HL1-lg.jpg",
+"name":"37276",
+"label":"37276"
+},
+{
+"image_sm":"Images/LightDarkSeries/37277-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37277-lg.jpg",
+"name":"37277",
+"label":"37277"
+},
+{
+"image_sm":"Images/LightDarkSeries/7021 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/7021 HL1-lg.jpg",
+"name":"7021",
+"label":"7021"
+},
+{
+"image_sm":"Images/LightDarkSeries/37262 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37262 HL1 HL2-sm.jpg",
+"name":"37262",
+"label":"37262"
+},
+{
+"image_sm":"Images/LightDarkSeries/37123 HL2 HL3-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37123 HL2 HL3-lg.jpg",
+"name":"37123",
+"label":"37123"
+},
+{
+"image_sm":"Images/LightDarkSeries/3054 HL1-5-sm.jpg",
+"image_lg":"Images/LightDarkSeries/3054 HL1-5-lg.jpg",
+"name":"3054",
+"label":"3054"
+},
+{
+"image_sm":"Images/LightDarkSeries/37237 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37237 HL1-sm.jpg",
+"name":"37237",
+"label":"37237"
+},
+{
+"image_sm":"Images/LightDarkSeries/37256 HL1-4-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37256 HL1-4-sm.jpg",
+"name":"37256",
+"label":"37256"
+},
+{
+"image_sm":"Images/LightDarkSeries/37281 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37281 HL1-lg.jpg",
+"name":"37281",
+"label":"37281"
+},
+{
+"image_sm":"Images/LightDarkSeries/3001 HL4-sm.jpg",
+"image_lg":"Images/LightDarkSeries/3001 HL4-lg.jpg",
+"name":"dup",
+"label":"3001"
+},
+{
+"image_sm":"Images/LightDarkSeries/37258 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37258 HL1-sm.jpg",
+"name":"37258",
+"label":"37258"
+},
+{
+"image_sm":"Images/LightDarkSeries/37276 HL3-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37276 HL3-lg.jpg",
+"name":"dup",
+"label":"37276"
+},
+{
+"image_sm":"Images/LightDarkSeries/37279-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37279-lg.jpg",
+"name":"37279",
+"label":"37279"
+},
+{
+"image_sm":"Images/LightDarkSeries/37264 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37264 HL1 HL2-sm.jpg",
+"name":"37264",
+"label":"37264"
+},
+{
+"image_sm":"Images/LightDarkSeries/37274 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37274 HL1-sm.jpg",
+"name":"37274",
+"label":"37274"
+},
+{
+"image_sm":"Images/LightDarkSeries/37127 HL2 HL3-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37127 HL2 HL3-lg.jpg",
+"name":"37127",
+"label":"37127"
+},
+{
+"image_sm":"Images/LightDarkSeries/37281 HL5-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37281 HL5-lg.jpg",
+"name":"dup",
+"label":"37281"
+},
+{
+"image_sm":"Images/LightDarkSeries/37266 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37266 HL1 HL2-sm.jpg",
+"name":"37266",
+"label":"37266"
+},
+{
+"image_sm":"Images/LightDarkSeries/37269 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37269 HL1 HL2-sm.jpg",
+"name":"37269",
+"label":"37269"
+},
+{
+"image_sm":"Images/LightDarkSeries/3044 HL1-6-sm.jpg",
+"image_lg":"Images/LightDarkSeries/3044 HL1-6-lg.jpg",
+"name":"3044",
+"label":"3044"
+},
+{
+"image_sm":"Images/LightDarkSeries/37179 HL4-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37179 HL4-sm.jpg",
+"name":"37179",
+"label":"37179"
+},
+{
+"image_sm":"Images/LightDarkSeries/5075 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/5075 HL1-lg.jpg",
+"name":"5075",
+"label":"5075"
+},
+{
+"image_sm":"Images/LightDarkSeries/37235 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37235 HL1-sm.jpg",
+"name":"37235",
+"label":"37235"
+},
+{
+"image_sm":"Images/LightDarkSeries/37268 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37268 HL1 HL2-sm.jpg",
+"name":"37268",
+"label":"37268"
+},
+{
+"image_sm":"Images/LightDarkSeries/37257 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37257 HL1-sm.jpg",
+"name":"37257",
+"label":"37257"
+},
+{
+"image_sm":"Images/LightDarkSeries/37246 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37246 HL1-sm.jpg",
+"name":"37246",
+"label":"37246"
+},
+{
+"image_sm":"Images/LightDarkSeries/37271 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37271 HL1-sm.jpg",
+"name":"37271",
+"label":"37271"
+},
+{
+"image_sm":"Images/LightDarkSeries/37267 HL1 HL2-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37267 HL1 HL2-sm.jpg",
+"name":"37267",
+"label":"37267"
+},
+{
+"image_sm":"Images/LightDarkSeries/37281 HL7-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37281 HL7-lg.jpg",
+"name":"dup",
+"label":"37281"
+},
+{
+"image_sm":"Images/LightDarkSeries/37280-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37280-lg.jpg",
+"name":"37280",
+"label":"37280"
+},
+{
+"image_sm":"Images/LightDarkSeries/37238 HL1-sm.jpg",
+"image_lg":"Images/LightDarkSeries/37238 HL1-sm.jpg",
+"name":"37238",
+"label":"37238"
 }
 ]
