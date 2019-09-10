afc849bb8866dbbbcee3c7c91dfdda2444e72681
pdhaval <dhavalpatel738@gmail.com>
2019-09-10 19:57:54 +0530

    Email change to [at] and [com] in contactus

--- ContactUs.php
+++ ContactUs.php
@@ -185,7 +185,7 @@
 
             <h5 class="card-title"><i class="fa fa-address-book" aria-hidden="true"></i> Address</h5>
             <p class="card-text"><span style="color: red;"><strong>SONY </strong></span>Ceramics, Matel Road, Dhuva-363622</p>
-            <p class="card-text"><i class="fa fa-envelope-open-o" aria-hidden="true"></i> office@sonyceramics.com</p>
+            <p class="card-text"><i class="fa fa-envelope-open-o" aria-hidden="true"></i> office [at] sonyceramics [dot] com</p>
 
         </div>
       </div>
