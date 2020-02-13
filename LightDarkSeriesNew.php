408cad6ffa163bb20b675979d5f000bcb2dfc900
pdhaval <dhavalpatel738@gmail.com>
2020-02-13 21:20:22 +0530

    Remove unnecessary light dark files
    - fix width and height of image
    - no hidden files

--- LightDarkSeriesNew.php
+++ LightDarkSeriesNew.php
@@ -12,34 +12,50 @@
     <?php
       echo file_get_contents(HEADER_NAVBAR_REDIRECTS);
 
+      //return array with file names not starting with '.'
+      function not_hidden($path) {
+          $files = scandir($path);
+          foreach($files as $file) {
+              if ($file[0] != '.') $nothidden[] = $file;
+            }
+          return $nothidden;
+      }
+
       $directory = LIGHTDARK_IMAGES_DIR;
-      $items = scandir($directory);
-      rsort($items);
-      //print_r($items);
+      $items = not_hidden($directory);
+      //rsort($items);
       $numOfFiles = count($items);
+
     ?>
 
     <?php
+
     $numberOfColumns = 6;
     $bootstrapColWidth = 12 / $numberOfColumns ;
 
     $arrayChunks = array_chunk($items, $numberOfColumns);
     foreach($arrayChunks as $items) {
-        echo '<div class="row">';
+        echo '<div class="row mx-4">';
+        echo '<div class="card-deck">';
         foreach($items as $item) {
+          if(strcmp(explode(".", $item)[1],"jpg") == 0) {
+            //strcmp returns 0 on equal
             echo '<div class="col-sm-'.$bootstrapColWidth.'">';
             echo '
                   <div class="card">
-                      <a class="image-popup-no-margins" href="Images/LightDarkSeries/'.$item.'">
-                          <img class="card-img-top" src="Images/LightDarkSeries/'.$item.'" alt="Card image cap">
+                      <a class="image-popup-no-margins embed-responsive embed-responsive-1by1" href="Images/LightDarkSeries/'.$item.'">
+                          <img class="card-img-top embed-responsive-item" src="Images/LightDarkSeries/'.$item.'" alt="Card image cap">
                       </a>
                       <h5 class="card-title" style="text-align: left;">'.explode(" ", $item)[0].'</h5>
                   </div>
             ';
             echo '</div>';
+          }
         }
         echo '</div>';
+        echo '</div>';
     }
+
     ?>
 
     <?php
