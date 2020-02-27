cf291b5db14733ed8dc3c510cad04965829acec9
pdhaval <dhavalpatel738@gmail.com>
2020-02-27 20:20:06 +0530

    remove unnecessary css class
    1. temporary added image files

--- LightDarkSeriesNew.php
+++ LightDarkSeriesNew.php
@@ -33,20 +33,21 @@
     $numberOfColumns = 6;
     $bootstrapColWidth = 12 / $numberOfColumns ;
 
-    $arrayChunks = array_chunk($items, $numberOfColumns);
-    foreach($arrayChunks as $items) {
+    //$arrayChunks = array_chunk($items, $numberOfColumns);
+    //foreach($arrayChunks as $items) {
         echo '<div class="row mx-4">';
-        echo '<div class="card-deck">';
+        // echo '<div class="card-deck">';
         foreach($items as $item) {
           if(strcmp(explode(".", $item)[1],"jpg") == 0) {
             //strcmp returns 0 on equal
-            echo '<div class="col-sm-'.$bootstrapColWidth.'">';
+            //echo '<div class="col-sm-'.$bootstrapColWidth.'">';
+            echo '<div class="col-sm-2 mb-3">';
             echo '
                   <div class="card">
-                      <a class="image-popup-no-margins embed-responsive embed-responsive-1by1" href="Images/LightDarkSeries/'.$item.'">
-                          <img class="card-img-top embed-responsive-item" src="Images/LightDarkSeries/'.$item.'" alt="Card image cap">
+                      <a class="image-popup-no-margins" href="Images/LightDarkSeries/'.$item.'">
+                          <img class="card-img-top" src="Images/LightDarkSeries/'.$item.'" alt="Card image cap">
                       </a>
-                      <h5 class="card-title" style="text-align: left;">'.explode(" ", $item)[0].'</h5>
+                      <h5 class="card-text" style="text-align: left;">'.explode(" ", $item)[0].'</h5>
                   </div>
             ';
             echo '</div>';
@@ -54,7 +55,7 @@
         }
         echo '</div>';
         echo '</div>';
-    }
+    //}
 
     ?>
 
