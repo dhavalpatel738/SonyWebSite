a4d762b9c1261f8d263edaa65d2864db786201d3
pdhaval <dhavalpatel738@gmail.com>
2021-02-14 21:29:48 +0530

    pinterest verify to header.php

--- components/php/header.php
+++ components/php/header.php
@@ -11,6 +11,9 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+<!-- Pinterest Verify -->
+<meta name="p:domain_verify" content="eac46b6098e808cc2d11fe1df4e3cfef"/>
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="/assets/font-awesome/css/font-awesome.min.css">
 <link rel="stylesheet" href="/assets/css/jquery-ui.css">
